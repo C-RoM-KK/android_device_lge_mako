cdfcd8d0db0456132777a536634ced243cfd5264
aways <aways@c-rom.org>
2014-06-17 18:56:46 +0200

    Set it for new vendor

--- crom.mk
+++ crom.mk
@@ -1,36 +1,25 @@
-# Name
-PRODUCT_RELEASE_NAME := N4
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# device
-$(call inherit-product-if-exists, device/lge/mako/full_mako.mk)
-
-# languages
-PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
-
-# phone
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/crom/config/common_phone.mk)
 
-# products
+# Enhanced NFC
+$(call inherit-product, vendor/crom/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := mako
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_BRAND := Google
 PRODUCT_NAME := crom_mako
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=crom.mako.$(shell date +%m%d%y).$(shell date +%H%M%S)
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
-# overrides
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_NUMBER=937116 \
-    PRODUCT_NAME=occam \
-    TARGET_BUILD_TYPE=user \
-    BUILD_VERSION_TAGS=release-keys \
-    PRIVATE_BUILD_DESC="occam-user 4.4.2 KOT49H 937116 release-keys" \
-    BUILD_FINGERPRINT="google/occam/mako:4.4.2/KOT49H/937116:user/release-keys"
-
-# bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/crom/prebuilt/common/media/xhdpi/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.4.2 KOT49H 937116 release-keys"
